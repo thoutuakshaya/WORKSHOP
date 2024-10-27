3f4fb07be31294228e5e540084d605b0eaac3038
thoutuakshaya <thoutuakshaya1@gmail.com>
2024-10-27 11:52:30 +0530

    27-10-2024

---
+++ react/project2/src/ModuleExportSample.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const  Module1= () => {
+  return (
+    <div>this is first module or function</div>
+  )
+}
+const  Module2= () => {
+    return (
+      <div>this is second module or function</div>
+    )
+  }
+
+export {Module1,Module2} 
