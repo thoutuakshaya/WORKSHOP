b6038818b153aef2700f66e415a1ff4f578ff750
thoutuakshaya <thoutuakshaya1@gmail.com>
2024-11-18 18:39:44 +0530

    Delete react/project2 directory

--- react/project2/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client'
-
-// import App from './App'
-import App from './App'
-import './assets/css/style.css'
-
-createRoot(document.getElementById('root')).render(
-  <App />
-)
